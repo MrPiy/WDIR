0cb48699e63a6be2bbbc4566f7e79b83a95cfd28
Xiaosea <maysrp@gmail.com>
2018-01-21 09:13:14 +0800

    Update download.php

--- download.php
+++ download.php
@@ -2,7 +2,7 @@
     session_start();
     include_once 'dclass.php';
     $DF=new share();
-    if($_SESSION['user'] || $file=$DF->file($_SESSION['file'])){//验证user
+    if($_SESSION['user'] || $file=$DF->file($_SESSION['file'],$_GET['file'])){//验证user
             header("Content-Type: application/octet-stream");
             header('X-Accel-Redirect: /down/'.$_GET['file']);//跳转到 down
     }else{
